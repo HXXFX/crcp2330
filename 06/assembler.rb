8a59733d1e15667fef6b9359b2296d28bd413209
HZxian <nikeplus@live.cn>
2015-11-16 14:29:16 -0600

    change a litte bit

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,1 +1,10 @@
-puts "Hello Zongxian!!! WASSUPPP!"
+# Expect the path and name to an .asm file.
+# Open that .asm file
+# Print the contents of that .asm file to the console (use puts)
+
+# Tip: Ruby File I/O
+# Tip: ARGV
+
+file = File.open("add/Add.asm", "r")
+contents = file.read
+puts contents
