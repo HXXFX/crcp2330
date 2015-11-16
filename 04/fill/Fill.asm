578b90f2e2984974a1a4cbf64b3612c8377fd91c
HZxian <nikeplus@live.cn>
2015-11-16 12:47:30 -0600

    Retro: Add older work.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,62 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(END)
+
+@KBD
+D=M
+
+@FILLBLACK
+D;JNE
+@FILLWHITE
+D;JEQ
+
+
+(FILLBLACK)
+@8191
+D=A
+@counterb
+M=D
+
+(DRAWBLACK)
+@counterb
+D=M
+@END
+D;JLT //finish DRAW and jump out 
+
+@SCREEN
+A=A+D
+M=-1
+
+@counterb
+M=M-1
+
+@DRAWBLACK
+0;JMP
+
+(FILLWHITE)
+@8191
+D=A
+@counterw
+M=D
+
+(DRAWWHITE)
+@counterw
+D=M
+@END
+D;JLT //finish DRAW and jump out 
+
+@SCREEN
+A=A+D
+M=0
+
+@counterw
+M=M-1
+
+@DRAWWHITE
+0;JMP
+
+
+@END
+0;JMP
