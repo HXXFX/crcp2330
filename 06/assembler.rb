b8aa9cb0bb5edeb1f6652046842693f4facfe14b
HZxian <nikeplus@live.cn>
2015-11-22 11:43:13 -0600

    change

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -5,7 +5,38 @@
 # Tip: Ruby File I/O
 # Tip: ARGV
 
-file = File.open(ARGV[0], "r")
-contents = file.read
-puts contents
+begin
+input_file = File.open(ARGV[0], "r")
+rescue => e
+put e
+end
 
+asm_lines = []
+input_file.readlines.each do |line|
+	asm_lines << line
+end
+
+def dont_read_this?(line)
+	line.include?("//") || line.empty?
+end
+
+asm_instructions = []
+asm_lines.each do |line|
+	line.strip!
+	asm_instructions << line unless dont_read_this? line
+end
+
+
+
+
+
+def name_output_file outputfile
+	outputfile.delete()
+
+end
+
+puts asm_instructions
+output_file_name = "#{ARGV[0]}.jack"
+output_file = File.open(output_file_name, "w")
+output_file.puts asm_instructions
+output_file.close
