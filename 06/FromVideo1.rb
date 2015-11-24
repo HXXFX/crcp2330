6bb15bf99d4b5cae807d78e5db3651b9b1b4bf58
HZxian <nikeplus@live.cn>
2015-11-24 15:40:47 -0600

    finsih video 6.5.1

--- 06/FromVideo1.rb
+++ 06/FromVideo1.rb
@@ -19,13 +19,35 @@ end
 	 	abort(" Do this way MOTHERF*CKER-> ruby ThisFileName.rb GoodShit.asm")
 	end
 
-AsmFileName = ARGV[0]
+asm_filename = ARGV[0]
 
-	unless arg_readable?(AsmFileName)
-		abort(" #{AsmFileName} not found or is unreadable")
+	unless arg_readable?(asm_filename)
+		abort(" #{asm_filename} not found or is unreadable")
 	end
 		
-	puts " The contents of #{AsmFileName}"
+	puts " The contents of #{asm_filename}"
 
-AsmFile = File.open(AsmFileName)
-puts AsmFile.read #read give the content of the file
+#asm_file = File.open(asm_filename)
+#puts asm_file.read #read give the content of the file
+
+File.open(asm_filename) do |asm_file|
+	asm_basename = File.basename(asm_filename, '.asm') #basename only keep basename delete .
+	path = File.split(asm_filename)[0] #split [0] get the path other then file name
+	hack_filename = "#{path}/#{asm_basename}.hack" 
+	File.open(hack_filename, 'w') do |hack_file|
+	assembler = Assembler.new(asm_file, hack_file)
+	assembler.assemble!
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
