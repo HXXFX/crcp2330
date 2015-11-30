e4e63fcbfb967b2df4e10d4e58c9b8fc66053672
HZxian <nikeplus@live.cn>
2015-11-30 10:48:17 -0600

    finsih hash table UNTESTED

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -32,37 +32,37 @@ class Assembler
 
 end#end Assembler class
 
-def arg_valid?
-	ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
-end
+	def arg_valid?
+		ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
+	end	
 
-def arg_readable?(path)
-	File.readable?(path)	
-end
+	def arg_readable?(path)
+		File.readable?(path)	
+	end	
 
-def hack_filename(asm_filename)
-	asm_basename = File.basename(asm_filename, '.asm') #basename only keep basename delete .
-	path = File.split(asm_filename)[0] #split [0] get the path other then file name
-	"#{path}/#{asm_basename}.hack" 
-end
+	def hack_filename(asm_filename)
+		asm_basename = File.basename(asm_filename, '.asm') #basename only keep basename delete .
+		path = File.split(asm_filename)[0] #split [0] get the path other then file name
+		"#{path}/#{asm_basename}.hack" 
+	end	
 
-	unless arg_valid?
-	 	abort(" Do this way MOTHERF*CKER-> ruby ThisFileName.rb GoodShit.asm")
+		unless arg_valid?
+		 	abort(" Do this way MOTHERF*CKER-> ruby ThisFileName.rb GoodShit.asm")
+		end	
+
+	asm_filename = ARGV[0]	
+
+		unless arg_readable?(asm_filename)
+			abort(" #{asm_filename} not found or is unreadable")
+		end
+			
+		#puts " The contents of #{asm_filename}"
+	#asm_file = File.open(asm_filename)
+	#puts asm_file.read #read give the content of the file	
+
+	File.open(asm_filename) do |asm_file|
+		File.open(hack_filename(asm_filename), 'w') do |hack_file|
+		assembler = Assembler.new(asm_file, hack_file)
+		assembler.assemble!
+		end
 	end
-
-asm_filename = ARGV[0]
-
-	unless arg_readable?(asm_filename)
-		abort(" #{asm_filename} not found or is unreadable")
-	end
-		
-	#puts " The contents of #{asm_filename}"
-#asm_file = File.open(asm_filename)
-#puts asm_file.read #read give the content of the file
-
-File.open(asm_filename) do |asm_file|
-	File.open(hack_filename(asm_filename), 'w') do |hack_file|
-	assembler = Assembler.new(asm_file, hack_file)
-	assembler.assemble!
-	end
-end
